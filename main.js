4e0057a8546a5d517299a30f15a493e145456013
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-19 18:56:26 +0100

    Finish alls Functions and Entire Main.JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // * VARIABLES / CONSTANTS
-let bill = Number(document.getElementById("bill").value);
-let numberOfPeople = Number(document.getElementById("numberOfPeople").value);
+let bill = document.getElementById("bill");
+let numberOfPeople = document.getElementById("numberOfPeople");
 let tips = document.getElementsByName("tips");
 let customTip = document.getElementById("custom");
 let amountPerson = document.getElementById("amountPerson");
@@ -10,28 +10,63 @@ let totalamount = document.getElementById("totalamount");
 const checkTip = () => {
   let Tip;
   tips.forEach((tip) => {
+    tip.addEventListener("change", (e) => {
+      calculateTotalPricePerPerson();
+    });
     if (tip.checked) {
       Tip = Number(tip.value);
+    } else if (customTip.value) {
+      Tip = Number(checkCustomTip());
     }
   });
   return Tip;
 };
 
-const checkCustomTip = (input) => {
-  if (input.value) {
-    return Number(input.value);
+const checkCustomTip = () => {
+  if (customTip.value) {
+    tips.forEach((tip) => {
+      if (tip.checked) {
+        tip.checked = false;
+      }
+    });
+    return Number(customTip.value).toFixed(2);
   }
   return;
 };
 
-const calculateAmount = () => {
+const calculateAmountPerPerson = () => {
   let result;
-  result = (bill * (checkTip() / 100)) / numberOfPeople;
-  return Number(result);
+  result =
+    (Number(bill.value) * (checkTip() / 100)) / Number(numberOfPeople.value);
+  result = Number(result).toFixed(2);
+  return Number((amountPerson.value = result));
 };
-const calculateTotal = () => {};
+
+const calculateTotalPricePerPerson = () => {
+  let total;
+  total =
+    Number(bill.value / numberOfPeople.value) + calculateAmountPerPerson();
+  total = Number(total).toFixed(2);
+  return Number((totalamount.value = total));
+};
 
 // * Function CALLS
-checkTip(); // ! WORKS
-checkCustomTip(customTip); // ! WORKS
-// calculateAmount(); // TODO
+// checkTip(); // ! WORKS
+// checkCustomTip(); // ! WORKS
+// calculateAmount(); // ! WORKS
+
+// * EVENTLISTENER
+
+bill.addEventListener("input", (e) => {
+  calculateTotalPricePerPerson();
+});
+
+numberOfPeople.addEventListener("input", (e) => {
+  calculateTotalPricePerPerson();
+});
+
+customTip.addEventListener("input", (e) => {
+  calculateTotalPricePerPerson();
+});
+
+// checkTip(); nested Eventlistener !
